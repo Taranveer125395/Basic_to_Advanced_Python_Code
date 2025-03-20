0d8d78d1a9128f00ff94ac973656130a1df10437
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-20 21:36:21 +0530

    Sum of Two Numbers a and b

---
+++ sum_of_two_number.py
@@ -0,0 +1,7 @@
+a = int(input('Enter Your 1st Number: ')) #1st value
+b = int(input('Enter Your 2nd Number: ')) #2nd value
+c = a + b   #sum of 1st value and 2nd value
+print('---------------------------------------')
+print(f'Given 1st Value: {a}')
+print(f'Given 2nd Value: {b}')
+print(f'Sum of two numbers {a} and {b} is {c}.')
