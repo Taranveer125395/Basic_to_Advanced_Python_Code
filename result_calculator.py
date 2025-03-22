b53da92484a4c930b79c99f2b65e8b2532da53e2
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-22 21:41:52 +0530

    Result Calculation for all Subjects

---
+++ result_calculator.py
@@ -0,0 +1,80 @@
+a = int(input('Enter Your Punjabi Marks: '))
+b = int(input('Enter Your English Marks: '))
+c = int(input('Enter Your Mathematics Marks: '))
+d = int(input('Enter Your Physics Marks: '))
+e = int(input('Enter Your Chemistry Marks: '))
+f = a + b + c + d + e
+g = (f/500)*100
+print('---------------Result Details----------------')
+print(f'Punjabi: {a}')
+print(f'English: {b}')
+print(f'Mathematics: {c}')
+print(f'Physics: {d}')
+print(f'Chemistry: {e}')
+if g < 33:
+    print('Fail in All Subjects')
+    print('Result: Fail')
+elif a < 33 and b < 33 and c < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and b < 33 and d < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and b < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and c < 33 and d < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and c < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and d < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif b < 33 and c < 33 and d < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif b < 33 and c < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif b < 33 and d < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif c < 33 and d < 33 and e < 33:
+    print('Fail in Examination & Repeat the Class')
+    print('Result: Fail')
+elif a < 33 and b < 33:
+    print('Reappear in Punjabi and English')
+elif a < 33 and c < 33:
+    print('Reappear in Punjabi and Mathematics')
+elif a < 33 and d < 33:
+    print('Reappear in Punjabi and Physics')
+elif a < 33 and e < 33:
+    print('Reappear in Punjabi and Chemistry')
+elif b < 33 and c < 33:
+    print('Reappear in English and Mathematics')
+elif b < 33 and d < 33:
+    print('Reappear in English and Physics')
+elif b < 33 and e < 33:
+    print('Reappear in English and Chemistry')
+elif c < 33 and d < 33:
+    print('Reappear in Mathematics and Physics')
+elif c < 33 and e < 33:
+    print('Reappear in Mathematics and Chemistry')
+elif d < 33 and e < 33:
+    print('Reappear in Physics and Chemistry')
+elif a < 33:
+    print('Reappear in Punjabi')
+elif b < 33:
+    print('Reappear in English')
+elif c < 33:
+    print('Reappear in Mathematics')
+elif d < 33:
+    print('Reappear in Physics')
+elif e < 33:
+    print('Reappear in Chemistry')
+else:
+    print(f'Total: {f}')
+    print(f'Percentage: {g}')
+    print('Result: PASS')
