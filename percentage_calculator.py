92ef9174deba72a21ca20651a47959176a5d7c05
Taranveer125395 <taranveer.s.kandhola@gmail.com>
2025-03-22 20:46:03 +0530

    Percentage Calculator of Two Number's Sum out of 200

---
+++ percentage_calculator.py
@@ -0,0 +1,11 @@
+a = int(input('Enter Your Marks Out of 100: '))
+b = int(input('Enter Your Marks Out of 100: '))
+c = a + b
+d = 200
+e = c / d
+f = e * 100
+print('----------------------------------------------------------')
+print(f'1st Marks out of 100 is {a}.')
+print(f'2nd Marks out of 100 is {b}.')
+print(f'Total Marks out of {d} is {c}.')
+print(f'Percentage of {c}(sum of {a} and {b}) out of {d} is {f}%')
